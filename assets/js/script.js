47caf322953e6e29d480e7abd6b2737c27ec720a
thisisverycool <kiranar2803@outlook.com>
2024-03-28 12:46:41 +0530

    tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-if (localStorage.getItem('theme' === null)) {
+if (localStorage.getItem('theme') == null) {
     localStorage.setItem('theme', 'light');
 } else {
     document.body.classList.add(localStorage.getItem('theme'))
 }
+
+document.getElementById("defaultOpenTab").click();
 
 function toggleNav() {
     var x = document.getElementById("navbarr");
@@ -23,3 +25,16 @@ function toggleTheme() {
         localStorage.setItem('theme', "light");
     }
 }
+function toggleTab(evt, tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " active";
+  } 
